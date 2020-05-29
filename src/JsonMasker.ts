e4354ad1454c051c0fc3ac8290007be6082b683b
Leah Domas <leah.domas@acadian.com>
2020-05-29 16:12:36 -0500

    WIP JSON Masker

--- src/JsonMasker.ts
+++ src/JsonMasker.ts
@@ -7,29 +7,72 @@ class JsonMasker {
     return this.maskDataHelper(json);
   }
   private maskDataHelper(json: any) {
-    if(Array.isArray(json)){
-      this.maskList(json);
-    } else if (json.isJSON) {
-      this.maskJson(json);
+    if(Array.isArray(json)) {
+      return this.maskList(json);
     } else {
-
+      return this.maskJson(json);
     }
   }
   maskList(list: any[]): any[] {
-    return [];
+    return list.map((element) => {
+      if(Array.isArray(element)){
+        return this.maskList(element);
+      } else if(jc.isJSON(element)){
+        return this.maskJson(element);
+      } else if(isNaN(element)){
+        return this.maskString(element);
+      } else {
+        return this.maskNumber(element);
+      }
+    })
   }
   maskJson(json: any): any {
-      if (Array.isArray(json)){
-        this.maskList(json)
+    const jsonArray = jr.toKeyValArray(json);
+    return jr.fromKeyValArray(jsonArray.map((element) => {
+      if (Array.isArray(element.value)) {
+        return {key: element.key, value: this.maskList(element.value)};
+      } else if (isNaN(element.value)){
+        return {key: element.key, value: this.maskString(element.value)};
+      } else {
+        return {key: element.key, value: this.maskNumber(element.value)};
       }
-    // jr.toKeyValArray;
-    // jr.fromKeyValArray;
+    }))
   }
   maskNumber(num: number): number {
-    return 0;
+    if (num.toString().length === 1){
+      return num * 11
+    } else {
+      let numString = num.toString();
+      let numStringArray = [];
+      let newNum;
+      if(num % 1 !== 0 && num < -1){
+        numStringArray = numString.split('');
+        numStringArray.shift();
+        numString = numStringArray.join();
+        numString.split('.');
+
+
+      } else if (num % 1  !== 0){
+
+      } else if (num < 1){
+
+      }else {
+        while (newNum !== Number(numString)){
+          numStringArray = numString.split('');
+          numStringArray.sort(() => Math.random() - 0.5);
+        }
+        return newNum;
+      }
+    }
   }
   maskString(str: string): string {
-    return '';
+    let newString;
+    while (newString !== str){
+      let stringArray = str.split('');
+      stringArray.sort(() => Math.random() - 0.5);
+      newString = stringArray.join();
+    }
+    return newString
   }
 }
 
