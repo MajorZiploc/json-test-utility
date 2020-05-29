44e16b44d1a56949c168f403ba1c00ceb4b531c8
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2020-05-29 16:38:35 -0500

    Addresses stuff.

--- src/JsonMasker.ts
+++ src/JsonMasker.ts
@@ -7,72 +7,86 @@ class JsonMasker {
     return this.maskDataHelper(json);
   }
   private maskDataHelper(json: any) {
-    if(Array.isArray(json)) {
+    if (Array.isArray(json)) {
       return this.maskList(json);
     } else {
       return this.maskJson(json);
     }
   }
+
   maskList(list: any[]): any[] {
-    return list.map((element) => {
-      if(Array.isArray(element)){
+    return list.map(element => {
+      if (Array.isArray(element)) {
         return this.maskList(element);
-      } else if(jc.isJSON(element)){
+      } else if (jc.isJSON(element)) {
         return this.maskJson(element);
-      } else if(isNaN(element)){
+      } else if (isNaN(element)) {
         return this.maskString(element);
       } else {
         return this.maskNumber(element);
       }
-    })
+    });
   }
+
   maskJson(json: any): any {
     const jsonArray = jr.toKeyValArray(json);
-    return jr.fromKeyValArray(jsonArray.map((element) => {
-      if (Array.isArray(element.value)) {
-        return {key: element.key, value: this.maskList(element.value)};
-      } else if (isNaN(element.value)){
-        return {key: element.key, value: this.maskString(element.value)};
-      } else {
-        return {key: element.key, value: this.maskNumber(element.value)};
-      }
-    }))
-  }
-  maskNumber(num: number): number {
-    if (num.toString().length === 1){
-      return num * 11
-    } else {
-      let numString = num.toString();
-      let numStringArray = [];
-      let newNum;
-      if(num % 1 !== 0 && num < -1){
-        numStringArray = numString.split('');
-        numStringArray.shift();
-        numString = numStringArray.join();
-        numString.split('.');
-
-
-      } else if (num % 1  !== 0){
-
-      } else if (num < 1){
-
-      }else {
-        while (newNum !== Number(numString)){
-          numStringArray = numString.split('');
-          numStringArray.sort(() => Math.random() - 0.5);
+    return jr.fromKeyValArray(
+      jsonArray.map(element => {
+        if (Array.isArray(element.value)) {
+          return { key: element.key, value: this.maskList(element.value) };
+        } else if (isNaN(element.value)) {
+          return { key: element.key, value: this.maskString(element.value) };
+        } else {
+          return { key: element.key, value: this.maskNumber(element.value) };
         }
-        return newNum;
-      }
-    }
+      })
+    );
   }
+
+  maskNumber(num: number): number {
+    const numStr = num.toString();
+    const matchList = numStr.match(/(-)?(\d+)(\.)?(\d*)/);
+    const sign = matchList[1] ?? '';
+    const wholeNumber = matchList[2] ?? '0';
+    const decemialPoint = matchList[3] ?? '';
+    const decemialValue = matchList[4] ?? '';
+
+    // if match group is not found, then it is undefined or ''?
+
+    // if (num.toString().length === 1) {
+    //   return num * 11;
+    // } else {
+    //   let numString = num.toString();
+    //   let numStringArray = [];
+    //   let newNum;
+    //   if (num % 1 !== 0 && num < -1) {
+    //     numStringArray = numString.split('');
+    //     numStringArray.shift();
+    //     numString = numStringArray.join();
+    //     numString.split('.');
+    //   } else if (num % 1 !== 0) {
+    //   } else if (num < 1) {
+    //   } else {
+    //     while (newNum !== Number(numString)) {
+    //       numStringArray = numString.split('');
+    //       numStringArray.sort(() => Math.random() - 0.5);
+    //     }
+    //     return newNum;
+    //   }
+    // }
+  }
+
   maskString(str: string): string {
-    let newString;
-    while (newString !== str){
-      let stringArray = str.split('');
-      stringArray.sort(() => Math.random() - 0.5);
-      newString = stringArray.join();
+    if (str === '') {
+      // pick a return char
+      //return it
     }
-    return newString
+    let newString;
+    while (newString !== str) {
+      let stringArray = str.split('');
+      newString = stringArray.sort(() => Math.random() - 0.5).join('');
+    }
+    return newString;
   }
 }
 
